629b91bf29e05f673e69756d4d0d5da813d9896c
Naeemo <naeemo@qq.com>
2022-09-10 08:50:29 +0800

    feat handle touchmove for mobile web

--- src/index.ts
+++ src/index.ts
@@ -20,21 +20,33 @@ const handlePointerDown = (pointerDownEvt: PointerEvent) => {
         return
     }
 
-    target.setPointerCapture(pointerDownEvt.pointerId)
-    const handleMove = (pointerMoveEvt: PointerEvent) => {
-        const xMoved = pointerMoveEvt.clientX - target.offsetLeft - xOffset
-        const yMoved = pointerMoveEvt.clientY - target.offsetTop - yOffset
+    function calculateTransform(pointerX: number, pointerY: number) {
+        const xMoved = pointerX - target.offsetLeft - xOffset
+        const yMoved = pointerY - target.offsetTop - yOffset
         const x = Math.min(Math.max(xMin, xMoved), xMax)
         const y = Math.min(Math.max(yMin, yMoved), yMax)
 
         target.style.transform = `translate3d(${x}px, ${y}px, 0)`
     }
+
+    target.setPointerCapture(pointerDownEvt.pointerId)
+    const handleMove = (pointerMoveEvt: PointerEvent) =>
+        calculateTransform(pointerMoveEvt.clientX, pointerMoveEvt.clientY)
+
+    const handleTouchMove = (touchMoveEvt: TouchEvent) => {
+        const clientX = touchMoveEvt.touches[0].clientX
+        const clientY = touchMoveEvt.touches[0].clientY
+        calculateTransform(clientX, clientY)
+    }
     const handleRelease = (pointerUpEvt: PointerEvent) => {
         target.releasePointerCapture(pointerUpEvt.pointerId)
         target.removeEventListener('pointermove', handleMove)
+        target.removeEventListener('touchmove', handleTouchMove)
         target.removeEventListener('pointerup', handleRelease)
     }
+
     target.addEventListener('pointermove', handleMove)
+    target.addEventListener('touchmove', handleTouchMove)
     target.addEventListener('pointerup', handleRelease)
 }
 
